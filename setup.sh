56816c68ad4abef44946f01bad876229fa0c4424
Raj Sudharshan <sudharshann05@gmail.com>
2023-10-20 01:26:12 -0700

    add packer template to create ami

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Ensure the script runs with root privileges
-if [ "$EUID" -ne 0 ]; then
-  echo "Run with root privileges."
-  exit 1
-fi
-
-# Update system's package list
-apt-get update
+# Update system's package list and upgrade
+sudo apt-get update && sudo apt-get upgrade -y
 
 # Install required packages for pyenv, build dependencies, and unzip
-apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git unzip
 
 # Install pyenv
 curl https://pyenv.run | bash
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
 
 # Configure the shell environment for pyenv
 echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
@@ -28,41 +27,29 @@ pyenv install 3.11.4
 pyenv global 3.11.4
 pyenv local 3.11.4
 
+python --version
+
+#remove git
+sudo apt-get remove --purge -y git
 
 # Add PostgreSQL repo and install PostgreSQL
-echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | tee /etc/apt/sources.list.d/pgdg.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
-apt-get update
-apt-get install -y postgresql-15
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update && sudo apt-get install -y postgresql-15
+
+# Ensure PostgreSQL service is started
+sudo service postgresql start
 
 # Configure PostgreSQL
-sudo -u postgres psql -c "CREATE USER raj WITH PASSWORD 'raj_pass05';"
+sudo -u postgres psql -c "CREATE USER raj WITH PASSWORD '$DB_PASSWORD';"
 sudo -u postgres psql -c "CREATE DATABASE csye6225_db OWNER raj;"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE csye6225_db TO raj;"
 
-# Set up the webapp
-#git clone https://github.com/RAJ-SUDHARSHAN/webapp.git
-#cd webapp/
+# Install Python packages
+pip install -r /tmp/requirements.txt
 
-python -m venv venv
-source venv/bin/activate
+# Move users.csv to /opt/
+sudo mv /tmp/users.csv /opt/
 
-pip install -r requirements.txt
-
-# Configure Flask
-export FLASK_APP=app
-export FLASK_DEBUG=True
-export DATABASE_URL=postgresql://raj:raj_pass05@localhost:5432/csye6225_db
-export CSV_PATH=/opt/users.csv
-
-flask db init
-flask db migrate -m "init"
-flask db upgrade
-flask populate_db
-flask run --host=0.0.0.0
-
-# Install and run ngrok
-# wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-# unzip ngrok-stable-linux-amd64.zip
-# ./ngrok http 5000
-
+ls -l /tmp/
+ls -la /opt/
