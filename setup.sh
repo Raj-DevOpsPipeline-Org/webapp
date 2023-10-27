d28b6fb48bb3917a9d044c2643536e2f27c53411
Raj Sudharshan Vidyalatha Natarajan <sudharshann05@gmail.com>
2023-10-26 21:39:59 -0700

    Feature/modify python installation (#17)
    * update dotenv to fetch env file from /opt/webapp.properties
    
    * remove env variables from packer file
    
    * remove env variables from packer file
    
    * update python installation in shell script
    
    * update python installation in shell script
    
    * update python installation in shell script

--- setup.sh
+++ setup.sh
@@ -3,14 +3,34 @@
 # Update system's package list and upgrade
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Install required packages
-sudo apt-get install -y build-essential libssl-dev zlib1g-dev wget \
-libreadline-dev libsqlite3-dev curl libbz2-dev xz-utils tk-dev libffi-dev \
-unzip python3.11 python3.11-venv python3-pip
+# Install required packages for pyenv, build dependencies, and unzip
+sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
+xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git unzip
 
-# Check Python version
-python3 --version
+# Install pyenv
+curl https://pyenv.run | bash
 
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+
+
+# Configure the shell environment for pyenv
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Install Python 3.11.4 using pyenv and set it as the default version
+pyenv install 3.11.4
+pyenv global 3.11.4
+pyenv local 3.11.4
+
+python --version
+
+#remove git
+sudo apt-get remove --purge -y git
 
 # Create the User and Group for the Application
 sudo groupadd csye6225
