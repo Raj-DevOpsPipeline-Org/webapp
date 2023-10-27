7f053d7b35340f1bdb94d6961ea24a3cb8551702
Raj Sudharshan <sudharshann05@gmail.com>
2023-10-27 03:29:10 -0700

    modify systemd service path

--- setup.sh
+++ setup.sh
@@ -3,34 +3,25 @@
 # Update system's package list and upgrade
 sudo apt-get update && sudo apt-get upgrade -y
 
-# Install required packages for pyenv, build dependencies, and unzip
+# Install required packages
 sudo apt-get install -y build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git unzip
+xz-utils tk-dev libffi-dev liblzma-dev python3-openssl unzip software-properties-common
 
-# Install pyenv
-curl https://pyenv.run | bash
-
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-
-
-# Configure the shell environment for pyenv
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init --path)"' >> ~/.bashrc
-echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
-source ~/.bashrc
-
-# Install Python 3.11.4 using pyenv and set it as the default version
-pyenv install 3.11.4
-pyenv global 3.11.4
-pyenv local 3.11.4
+# Add the Python PPA and install Python 3.11
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update
+sudo apt-get install -y python3.11 python3.11-venv
 
 python --version
 
 #remove git
 sudo apt-get remove --purge -y git
+
+# Create the User and Group for the Application
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
 
 # Create the User and Group for the Application
 sudo groupadd csye6225
