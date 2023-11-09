0c699588de51ae916b3bcc6002a8464f178761d0
Raj Sudharshan <sudharshann05@gmail.com>
2023-11-09 01:26:48 -0800

    add logging and cloudwatch metrics

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
+import logging
 import os
+from logging import FileHandler
 
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, Response, jsonify, request
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
-from sqlalchemy.exc import SQLAlchemyError
+from pythonjsonlogger import jsonlogger
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy_utils import create_database, database_exists
+from statsd import StatsClient
 
 from assignments import assignments_bp
 from extensions import auth
@@ -32,6 +36,22 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 app.register_blueprint(assignments_bp)
 
+# Initialize Logging
+file_handler = FileHandler("/var/log/webapp/csye6225.log")
+file_handler.setLevel(logging.INFO)
+
+formatter = jsonlogger.JsonFormatter(
+    fmt="%(asctime)s %(levelname)s %(message)s",
+    rename_fields={"asctime": "date_time", "levelname": "log_level"},
+)
+
+file_handler.setFormatter(formatter)
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
+
+# Statsd Metrics
+statsd = StatsClient("localhost", 8125)
+
 # Initialize the database
 db.init_app(app)
 
@@ -50,20 +70,27 @@ def database_health_check():
         "Content-Length": "0",
     }
 
+    app.logger.info(f"Health check endpoint called with method: {request.method}")
+
     if request.method != "GET":
+        app.logger.warning("Health check endpoint called with non-GET method")
         return Response(status=405, headers=headers)
 
     if request.data or request.form or request.args:
+        app.logger.warning("Health check endpoint called with unexpected data")
         return Response(status=400, headers=headers)
 
     try:
         connection = psycopg2.connect(os.getenv("DATABASE_URL"))
         connection.close()
+        app.logger.info("Database connection successful for health check")
         response = Response(status=200, headers=headers)
 
-    except Exception:
+    except Exception as e:
+        app.logger.error(f"Database connection failed for health check: {e}")
         response = Response(status=503, headers=headers)
 
+    statsd.incr("csye6225_endpoint_healthz_http_get")
     return response
 
 
@@ -74,6 +101,10 @@ def modify_headers(response):
     response.headers["Pragma"] = "no-cache"
     response.headers["X-Content-Type-Options"] = "nosniff"
     response.headers["content-type"] = "application/json"
+    endpoint = request.endpoint.split(".")[-1]
+    method = request.method.lower()
+    metric_name = f"csye6225_endpoint_{endpoint}_http_{method}"
+    statsd.incr(metric_name)
     return response
 
 
@@ -87,13 +118,13 @@ def create_tables():
 def populate_db_command():
     csv_path = os.getenv("CSV_PATH", "/opt/users.csv")
     if not csv_path:
-        print("CSV_PATH environment variable not set!")
+        app.logger.error("CSV_PATH environment variable not set!")
         return
     try:
         populate_db(csv_path, bcrypt)
-        print("Database populated!")
+        app.logger.info("Database populated successfully.")
     except Exception as e:
-        print(f"An error occurred: {e}")
+        app.logger.error(f"An error occurred while populating the database: {e}")
 
 
 # with app.app_context():
@@ -113,13 +144,22 @@ def verify_password(email, password):
         user = Account.query.filter_by(email=email).first()
         if user and check_password(user.password, password):
             return user
+        app.logger.warning(
+            "Authentication failed for user: {email}".format(email=email)
+        )
         return False
 
-    except SQLAlchemyError:
+    except SQLAlchemyError as e:
+        app.logger.error(
+            f"Database error occurred during authentication for user {email}: {e}"
+        )
         return jsonify({"message": "Database error occurred."}), 503
 
     except Exception as e:
+        app.logger.error(
+            f"Unexpected error during authentication for user {email}: {e}"
+        )
         return (
-            jsonify({"message": "Unable to fetch assignments."}),
+            jsonify({"message": "Unable to verify user."}),
             400,
         )
